de5705d1fa0934eda1b6f3a69bc214c929832628
Ryuto <koba.ryu0402@gmail.com>
2021-08-08 12:30:22 +0900

    Detail Part

--- script.js
+++ script.js
@@ -23,3 +23,15 @@ $(function() {
       }
   });
 });
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    //スクロールのスピード
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
